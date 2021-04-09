e354073f026d058832fe818acf221c15747e74a3
Thiago Cabral <ericson.thiago@gmail.com>
2021-04-09 16:01:40 -0300

    First commit

---
+++ src/main/java/br/com/treinamento/aunteticarusuario/controller/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.treinamento.aunteticarusuario.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
+		return "Hello "+name+"!!";
+	}
+}
---
+++ src/test/java/br/com/treinamento/aunteticarusuario/AunteticarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.aunteticarusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AunteticarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
